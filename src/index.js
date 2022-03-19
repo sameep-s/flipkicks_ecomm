69b007b4fe877a81649c1933b17588ef1cee2285
sameep-s <sameep.sharma98@gmail.com>
2022-03-19 19:17:57 +0530

    change minor

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+// import App from "./App";
+import Home from './pages/Home'
 // import { makeServer } from "./server";
 
 // Call make Server
@@ -9,7 +10,7 @@ import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
   document.getElementById("root")
 );
