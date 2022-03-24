71c257d28635bf2141c98370a71b55dc3561a091
sameep-s <sameep.sharma98@gmail.com>
2022-03-25 03:19:11 +0530

    .

--- src/util_fucntions/getFilteredDataSize.js
+++ src/util_fucntions/getFilteredDataSize.js
@@ -4,4 +4,5 @@ export const getFilteredDataSize = (productList, size = []) => {
         return productList.filter((item) => size.includes(item.size))
 
     return productList;
+    console.log(`cart functionality`);
 }
