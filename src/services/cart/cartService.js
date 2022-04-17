dc299818fc25d5ca31ff8ab87d6ae025f681e9df
sameep-s <sameep.sharma98@gmail.com>
2022-04-18 04:24:42 +0530

    refactor.

---
+++ src/services/cart/cartService.js
@@ -0,0 +1,60 @@
+import axios from "axios"
+
+
+export const addToCart = (dispatch_Cart, product, token) => {
+    try {
+        const { data: { cart } } = await axios.post('/api/user/cart', {
+            product
+        },
+            {
+                headers: {
+                    authorization: token,
+                }
+            }
+        );
+        cart && dispatch_Cart({ type: "CART_INIT", payload: { cart: cart } })
+    }
+    catch (e) {
+        console.error(e)
+    }
+}
+
+
+export const removeFromCart = (dispatch_Cart, id, token) => {
+    try {
+        const { data: { cart } } = await axios.delete(`/api/user/cart/${id}`,
+            {
+                headers: {
+                    authorization: token,
+                }
+            }
+        );
+        cart && dispatch_Cart({ type: "CART_INIT", payload: { cart: cart } })
+    }
+    catch (e) {
+        console.error(e)
+    }
+}
+
+
+export const updateQtyCart = (dispatch_Cart, id, action, token) => {
+    try {
+        const { data: { cart } } = await axios.post(`api/user/cart/${id}`,
+            {
+                action: {
+                    type: action === "INC_QTY" ? "increment" : "decrement",
+                },
+            },
+            {
+                headers: {
+                    authorization: token,
+                },
+            }
+        );
+
+        cart && dispatch_Cart({ type: "CART_INIT", payload: { cart: cart } })
+
+    } catch (e) {
+        console.error(e)
+    }
+}
