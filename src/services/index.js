7e96a1c6f93a324e5ff937fc0af50318487612ac
sameep-s <sameep.sharma98@gmail.com>
2022-04-18 04:26:15 +0530

    removed console.log

--- src/services/index.js
+++ src/services/index.js
@@ -1,8 +1,15 @@
 import { loginService } from "./auth/loginService";
 import { signupService } from "./auth/signupService";
+import { addToWishlist, removeFromWishlist } from "./wishlist/wishlistService";
+import { addToCart, removeFromCart, updateQtyCart } from "./cart/cartService";
 
 
 export {
     loginService,
-    signupService
+    signupService,
+    addToWishlist,
+    removeFromWishlist,
+    addToCart,
+    removeFromCart,
+    updateQtyCart,
 };
