1ae52917e72834af01c33bd060749b565e64062b
sameep-s <sameep.sharma98@gmail.com>
2022-06-17 21:24:09 +0530

    added dynamic values to cart.

--- src/pages/CardCart.jsx
+++ src/pages/CardCart.jsx
@@ -15,13 +15,13 @@ const CardCart = (product) => {
             <div className="cart-product-wrapper">
                 <div className="cart-product-container">
                     <div className="cart-product-image">
-                        <img src="https://images.unsplash.com/photo-1610870372593-a8647b04451f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="product-image" />
+                        <img src={product.image} alt="product-image" />
                     </div>
                     <div className="cart-product-info">
                         <div className="cart-info-cont-left">
                             <div className="cart-sub-cont1">
-                                <div className="cart-info-cont-heading">Nike Air Jordans1</div>
-                                <div className="cart-info-price">$6000</div>
+                                <div className="cart-info-cont-heading">{product.productName}</div>
+                                <div className="cart-info-price">${product.price} </div>
                                 <div className="cart-info-discount">50% off</div>
                             </div>
                             <div className="cart-sub-cont2">
